c8451e1d47281907a13ee5105edc6050a327388f
Roberto Leão <robertoleao@Air-de-Roberto.Lan>
2020-11-29 00:42:29 -0300

    atualizando comentarios

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,5 @@
+# permite provisionar uma seção isolada logicamente na qual é possível executar recursos da AWS em uma rede virtual que você mesmo define https://aws.amazon.com/pt/vpc/
+
 resource "aws_vpc" "My_VPC" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
